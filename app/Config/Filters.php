197d1f03d82cf980ce80adcd624c761e1d11490c
khtab <khtabmax78@gmail.com>
2023-05-12 19:34:33 +0400

    first commit

---
+++ app/Config/Filters.php
@@ -0,0 +1,116 @@
+<?php
+namespace Config;
+
+use CodeIgniter\Config\BaseConfig;
+
+class Filters extends BaseConfig
+{
+	// Makes reading things below nicer,
+	// and simpler to change out script that's used.
+	// public $aliases = [
+	// 	'csrf'     => \CodeIgniter\Filters\CSRF::class,
+	// 	'toolbar'  => \CodeIgniter\Filters\DebugToolbar::class,
+	// 	'honeypot' => \CodeIgniter\Filters\Honeypot::class,
+	// 	'auth' => \App\Filters\Auth::class,
+	// 	'noauth' => \App\Filters\Noauth::class,
+	// 	'userscheck' => \App\Filters\UsersCheck::class,
+	// ];
+
+    public array $aliases = [
+         'csrf'          => CSRF::class,
+         'toolbar'       => DebugToolbar::class,
+         'honeypot'      => Honeypot::class,
+         'invalidchars'  => InvalidChars::class,
+         'noauth' => \App\Filters\Noauth::class,
+         'secureheaders' => SecureHeaders::class,
+         'userscheck' => \App\Filters\UsersCheck::class,
+         'toolbar' => \CodeIgniter\Filters\DebugToolbar::class,
+     ];
+
+	// Always applied before every request
+	public $globals = [
+		'before' => [
+			'userscheck'
+			//'honeypot'
+			// 'csrf',
+		],
+		'after'  => [
+			'toolbar',
+			//'honeypot'
+		],
+	];
+
+	// Works on all of a particular HTTP method
+	// (GET, POST, etc) as BEFORE filters only
+	//     like: 'post' => ['CSRF', 'throttle'],
+	public $methods = [];
+
+	// List filter aliases and any before/after uri patterns
+	// that they should run on, like:
+	//    'isLoggedIn' => ['before' => ['account/*', 'profiles/*']],
+	public $filters = [];
+}
+
+
+// namespace Config;
+
+// use CodeIgniter\Config\BaseConfig;
+// use CodeIgniter\Filters\CSRF;
+// use CodeIgniter\Filters\DebugToolbar;
+// use CodeIgniter\Filters\Honeypot;
+// use CodeIgniter\Filters\InvalidChars;
+// use CodeIgniter\Filters\SecureHeaders;
+
+// class Filters extends BaseConfig
+// {
+//     /**
+//      * Configures aliases for Filter classes to
+//      * make reading things nicer and simpler.
+//      */
+//     public array $aliases = [
+//         'csrf'          => CSRF::class,
+//         'toolbar'       => DebugToolbar::class,
+//         'honeypot'      => Honeypot::class,
+//         'invalidchars'  => InvalidChars::class,
+//         'secureheaders' => SecureHeaders::class,
+//     ];
+
+//     /**
+//      * List of filter aliases that are always
+//      * applied before and after every request.
+//      */
+//     public array $globals = [
+//         'before' => [
+//             // 'honeypot',
+//             // 'csrf',
+//             // 'invalidchars',
+//         ],
+//         'after' => [
+//             'toolbar',
+//             // 'honeypot',
+//             // 'secureheaders',
+//         ],
+//     ];
+
+//     /**
+//      * List of filter aliases that works on a
+//      * particular HTTP method (GET, POST, etc.).
+//      *
+//      * Example:
+//      * 'post' => ['foo', 'bar']
+//      *
+//      * If you use this, you should disable auto-routing because auto-routing
+//      * permits any HTTP method to access a controller. Accessing the controller
+//      * with a method you don’t expect could bypass the filter.
+//      */
+//     public array $methods = [];
+
+//     /**
+//      * List of filter aliases that should run on any
+//      * before or after URI patterns.
+//      *
+//      * Example:
+//      * 'isLoggedIn' => ['before' => ['account/*', 'profiles/*']]
+//      */
+//     public array $filters = [];
+// }
